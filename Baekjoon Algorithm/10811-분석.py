f917c8e2b60f619827c84e62b6109cbbb8c0f876
cliffclimber-721 <cliffclimber@g.skku.edu>
2024-02-26 14:48:19 +0900

    assert grammar

--- Baekjoon Algorithm/10811-분석.py
+++ Baekjoon Algorithm/10811-분석.py
@@ -4,6 +4,7 @@ basket = [i for i in range(1, N+1)]
 for _ in range(M):
     b1, b2 = map(int, input().split())
     tmp = basket[b1-1:b2]
+    print(basket[b1-1:b2])
     tmp.reverse()
     basket[b1-1:b2] = tmp
 
--- Basic grammars/assert.py
+++ Basic grammars/assert.py
@@ -4,7 +4,7 @@
 lists = [1, 3, 6, 3, 8, 7, 13, 23, 13, 2, 3.14, 2, 3, 7]
 
 def test(t):
-    assert type(t) is int, 'not integer'
+    assert type(t) is int, 'not ints'
 
 for i in lists:
     test(i)
